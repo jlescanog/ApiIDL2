3d4616164b46022d08568997a37411f1adf31339
Jhair W L <jlescanog@outlook.com>
2024-05-27 18:27:54 -0500

    Agregado clase libros, falta agregar EntityFC

---
+++ ApiIDL2/Models/Libros.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiIDL2.Models
+{
+    public class Libros
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+    }
+}
